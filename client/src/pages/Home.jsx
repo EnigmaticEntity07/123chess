265bc88f42569ea39a3a28f8fbeff1e98bfba805
Advait <advait.vibhute@gmail.com>
2026-06-11 22:47:42 +0530

    fix: import useState in Home.jsx to fix app crash

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { API_URL } from '../config';
